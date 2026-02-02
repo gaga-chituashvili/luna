26c3586c31453b4978c9bc16b77a3496c3a44c92
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-02-02 22:45:53 +0400

    feat:add postgress_db host in render

--- luna-backend/src/app.js
+++ luna-backend/src/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const lunaRouter = require("./routes/luna-route");
+const sequelize = require("../utils/db"); 
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use("/v1", lunaRouter);
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database synchronized");
+  })
+  .catch((error) => {
+    console.log(`Could not connect to DB: ${error}`);
+  });
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:3000`);
