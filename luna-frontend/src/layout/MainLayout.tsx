2409932bd61b210502a63e2693b3b81c26ac2e94
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-02-13 18:07:35 +0400

    Fix: Resolve routing errors and add product creation functionality

--- luna-frontend/src/layout/MainLayout.tsx
+++ luna-frontend/src/layout/MainLayout.tsx
@@ -1,17 +1,17 @@
-import { Outlet } from 'react-router-dom'
-import { Header } from '../component/header/Header'
-import { Footer } from '../component/footer/Footer'
+import { Outlet } from "react-router-dom";
+import { Header } from "../component/header/Header";
+import { Footer } from "../component/footer/Footer";
 
 const MainLayout = () => {
   return (
     <>
-      <Header />
+      <Header/>
       <main>
         <Outlet />
       </main>
-      <Footer/>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
