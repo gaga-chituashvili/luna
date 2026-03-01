57ec1d7eb34af721b6642a763bffb6c238c6045a
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-03-01 22:40:56 +0400

    Add Cloudinary for cloud image storage

--- luna-backend/src/middlewares/upload.js
+++ luna-backend/src/middlewares/upload.js
@@ -1,19 +1,12 @@
 const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../../config/cloudinary");
 
-const uploadPath = "/opt/render/project/src/uploads";
-
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadPath);
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "luna-coffee",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
   },
 });
 
