e57a5675ec8a9bbbc1b0096bcf40fc303f8b3c70
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-03-04 20:08:14 +0400

    fix: implement loading state for coffees fetch in Shop page

--- luna-frontend/src/pages/Shop.tsx
+++ luna-frontend/src/pages/Shop.tsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
 import { ShopPageCoffeeCard } from "../component/coffees/ShopPageCoffeeCard";
+import CoffeesApi from "../api/api /coffees";
+import type { Coffee } from "../api/type/coffees";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
 export const Shop = () => {
-  return <ShopPageCoffeeCard />;
+  const [coffees, setCoffees] = useState<Coffee[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    CoffeesApi().then((data) => {
+      setCoffees(data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <ClimbingBoxLoader color="#cfbfbf" size={20} />;
+  }
+
+  return <ShopPageCoffeeCard allCoffees={coffees} />;
 };
