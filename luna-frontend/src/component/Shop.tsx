a4805fdf59fc024770bc6fd0f080ec389556f39a
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-03-13 16:57:44 +0400

    fix:add responsive design first shop in home page

--- luna-frontend/src/component/Shop.tsx
+++ luna-frontend/src/component/Shop.tsx
@@ -8,19 +8,22 @@ export const Shop = () => {
   return (
     <section
       className="
-        w-full
-        min-h-[80vh]
-        flex items-center justify-between
-        px-20
-        text-white
-        bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))]
-        from-[#2b1b16]
-        via-[#1a0f0c]
-        to-black
-      "
+    w-full
+    min-h-[80vh]
+    flex flex-col lg:flex-row
+    items-center justify-between
+    px-6 md:px-12 lg:px-20
+    gap-12
+    pt-24
+    text-white
+    bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))]
+    from-[#2b1b16]
+    via-[#1a0f0c]
+    to-black
+  "
     >
-      <article className="flex flex-col gap-y-10 max-w-xl">
-        <h2 className="text-5xl font-semibold leading-tight tracking-tight">
+      <article className="flex flex-col gap-y-10 max-w-xl text-center lg:text-left">
+        <h2 className="text-4xl md:text-5xl lg:text-5xl font-semibold leading-tight tracking-tight">
           Specialty{" "}
           <span className="px-4 py-1 bg-white text-[#6b3f2b] rounded-full">
             coffee
@@ -30,16 +33,23 @@ export const Shop = () => {
           moments.
         </h2>
 
-        <div className="flex gap-x-4">
+        <div className="flex gap-x-4 justify-center lg:justify-start">
           <Button variant="default">Shop Coffee</Button>
 
-          <Button onClick={() => navigate({ to: ROUTES.ourStory })} variant="secondary">
+          <Button
+            onClick={() => navigate({ to: ROUTES.ourStory })}
+            variant="secondary"
+          >
             Our Story
           </Button>
         </div>
       </article>
 
-      <img src={coffee} alt="coffeeimg" className="drop-shadow-2xl" />
+      <img
+        src={coffee}
+        alt="coffeeimg"
+        className="drop-shadow-2xl w-64 md:w-80 lg:w-auto"
+      />
     </section>
   );
 };
